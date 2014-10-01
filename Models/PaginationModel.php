757f17cdac8f901227bb934df621dc6f963e9105
ovaljamestrusler <james.trusler@oval.uk.com>
2014-10-01 15:58:35 +0100

    Removed need for this in controller

--- Models/PaginationModel.php
+++ Models/PaginationModel.php
@@ -4,5 +4,10 @@
         public $page;
         public $amount;
         public $columns = array();
+        
+        public function __Construct()
+        {
+            $this->page = Input::has( "page" ) ? Input::get( "page" ) : 0;
+        }
     }
 ?>
